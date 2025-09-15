ad2217038ae169ea5b0d4a2eb56623b8ca45e04f
Arup3201 <arupjana7365@gmail.com>
2025-09-15 20:31:05 +0530

    update: binary search working algorithm and test

--- search.go
+++ search.go
@@ -1,5 +1,20 @@
 package search
 
-func BinarySearch(nums []int, target int) int {
-	return 0
+func BinarySearch(nums []int, target int) (int, bool) {
+	n := len(nums)
+	l, h := 0, n-1
+
+	for l <= h {
+		mid := l + (h-l)/2
+
+		if nums[mid] == target {
+			return mid, true
+		} else if target > nums[mid] {
+			l = mid + 1
+		} else {
+			h = mid - 1
+		}
+	}
+
+	return -1, false
 }
--- search_test.go
+++ search_test.go
@@ -7,11 +7,33 @@ func TestBinarySearch(t *testing.T) {
 		arr := []int{1, 3, 5, 10, 12, 15}
 		x := 10
 
-		got := BinarySearch(arr, x)
+		got, _ := BinarySearch(arr, x)
 
 		want := 3
 		if got != want {
 			t.Errorf("BinarySearch failed, expected %d got %d", want, got)
+		}
+	})
+	t.Run("search 15 in [1, 3, 5, 10, 12, 15]", func(t *testing.T) {
+		arr := []int{1, 3, 5, 10, 12, 15}
+		x := 15
+
+		got, _ := BinarySearch(arr, x)
+
+		want := 5
+		if got != want {
+			t.Errorf("BinarySearch failed, expected %d got %d", want, got)
+		}
+	})
+	t.Run("search 30 in [1, 3, 5, 10, 12, 15]", func(t *testing.T) {
+		arr := []int{1, 3, 5, 10, 12, 15}
+		x := 30
+
+		_, got := BinarySearch(arr, x)
+
+		want := false
+		if got != want {
+			t.Errorf("BinarySearch failed, expected %t got %t", want, got)
 		}
 	})
 }
